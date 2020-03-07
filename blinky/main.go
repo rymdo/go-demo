ea71777015723d00343a0fc577148d7f0675aa7b
Niklas Wigertz <niklas.wigertz@briteback.com>
2020-03-07 16:05:31 +0100

    feat: add blinky example

---
+++ blinky/main.go
@@ -0,0 +1,20 @@
+// Source: https://github.com/tinygo-org/tinygo
+
+package main
+
+import (
+	"machine"
+	"time"
+)
+
+func main() {
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		led.Low()
+		time.Sleep(time.Millisecond * 250)
+
+		led.High()
+		time.Sleep(time.Millisecond * 250)
+	}
+}
---
+++ blinky/readme.md
@@ -0,0 +1,11 @@
+# blinky
+
+## Code source
+
+Copied from https://github.com/tinygo-org/tinygo
+
+## compile/flash
+
+```bash
+tinygo flash -target=<DEVICE> -port <PORT> .
+```
